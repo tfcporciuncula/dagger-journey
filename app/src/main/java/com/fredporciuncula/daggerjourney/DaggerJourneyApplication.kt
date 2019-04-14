656e585c0568454b40b5bb19df69cc5f6ace613d
Fred Porciúncula <fred@blinkist.com>
2019-04-14 15:57:04 +0200

    3. Hold component in the application statically

--- app/src/main/java/com/fredporciuncula/daggerjourney/DaggerJourneyApplication.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/DaggerJourneyApplication.kt
@@ -1,13 +1,24 @@
 package com.fredporciuncula.daggerjourney
 
 import android.app.Application
+import com.fredporciuncula.daggerjourney.di.ApplicationComponent
+import com.fredporciuncula.daggerjourney.di.DaggerApplicationComponent
 import timber.log.Timber
 
 class DaggerJourneyApplication : Application() {
 
+  companion object {
+    lateinit var component: ApplicationComponent private set
+  }
+
   override fun onCreate() {
     super.onCreate()
+    initializeComponent()
     setupTimber()
+  }
+
+  private fun initializeComponent() {
+    component = DaggerApplicationComponent.create()
   }
 
   private fun setupTimber() {
--- app/src/main/java/com/fredporciuncula/daggerjourney/view/MainActivity.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/view/MainActivity.kt
@@ -2,12 +2,12 @@ package com.fredporciuncula.daggerjourney.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fredporciuncula.daggerjourney.DaggerJourneyApplication
 import com.fredporciuncula.daggerjourney.R
-import com.fredporciuncula.daggerjourney.di.DaggerApplicationComponent
 
 class MainActivity : AppCompatActivity() {
 
-  private val businessRunner = DaggerApplicationComponent.create().businessRunner
+  private val businessRunner = DaggerJourneyApplication.component.businessRunner
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
