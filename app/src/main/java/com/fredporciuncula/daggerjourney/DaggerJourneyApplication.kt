ef31444f5955bd9d52a934c2f0dd70c31a4fc0fc
Fred Porciúncula <fred@blinkist.com>
2019-04-14 15:02:01 +0200

    6. Bind application context with a builder

--- app/src/main/java/com/fredporciuncula/daggerjourney/DaggerJourneyApplication.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/DaggerJourneyApplication.kt
@@ -1,7 +1,6 @@
 package com.fredporciuncula.daggerjourney
 
 import android.app.Application
-import com.fredporciuncula.daggerjourney.di.ApplicationModule
 import com.fredporciuncula.daggerjourney.di.DaggerApplicationComponent
 import com.fredporciuncula.daggerjourney.di.InjectorProvider
 import timber.log.Timber
@@ -11,7 +10,7 @@ class DaggerJourneyApplication : Application(), InjectorProvider {
   override val component by lazy {
     DaggerApplicationComponent
       .builder()
-      .applicationModule(ApplicationModule(applicationContext))
+      .applicationContext(applicationContext)
       .build()
   }
 
--- app/src/main/java/com/fredporciuncula/daggerjourney/di/ApplicationComponent.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/di/ApplicationComponent.kt
@@ -2,17 +2,18 @@ package com.fredporciuncula.daggerjourney.di
 
 import android.content.Context
 import com.fredporciuncula.daggerjourney.business.BusinessRunner
+import dagger.BindsInstance
 import dagger.Component
-import dagger.Module
-import dagger.Provides
 import javax.inject.Singleton
 
-@Singleton
-@Component(modules = [ApplicationModule::class])
+@Singleton @Component
 interface ApplicationComponent {
+
+  @Component.Builder
+  interface Builder {
+    @BindsInstance fun applicationContext(applicationContext: Context): Builder
+    fun build(): ApplicationComponent
+  }
 
   val businessRunner: BusinessRunner
 }
-
-@Module
-class ApplicationModule(@get:Provides val applicationContext: Context)
