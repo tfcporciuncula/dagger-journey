2e2551790e9bf084e4734f72331c13bfb266ad55
Fred Porciúncula <fred@blinkist.com>
2019-04-20 11:19:22 +0200

    10. Introduce @Binds

--- app/src/main/java/com/fredporciuncula/daggerjourney/di/BusinessModule.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/di/BusinessModule.kt
@@ -2,12 +2,12 @@ package com.fredporciuncula.daggerjourney.di
 
 import com.fredporciuncula.daggerjourney.business.Business
 import com.fredporciuncula.daggerjourney.business.BusinessRunner
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-object BusinessModule {
+interface BusinessModule {
 
-  @JvmStatic @Provides
-  fun provideBusinessRunner(businessRunner: BusinessRunner): Business.Runner = businessRunner
+  @Binds
+  fun provideBusinessRunner(businessRunner: BusinessRunner): Business.Runner
 }
