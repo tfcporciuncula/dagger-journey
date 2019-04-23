116ab1398e1d06bd0236ba6e5ed84ccd4568e2ce
Fred Porciúncula <fred@blinkist.com>
2019-04-23 10:40:33 +0200

    15. Replicate extensions to fragments

--- app/src/main/java/com/fredporciuncula/daggerjourney/di/ViewModelFactoryExtensions.kt
+++ app/src/main/java/com/fredporciuncula/daggerjourney/di/ViewModelFactoryExtensions.kt
@@ -1,14 +1,34 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.fredporciuncula.daggerjourney.di
 
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-@Suppress("UNCHECKED_CAST")
 inline fun <reified T : ViewModel> FragmentActivity.viewModel(
   crossinline provider: () -> T
 ) = viewModels<T> {
+  object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.viewModel(
+  crossinline provider: () -> T
+) = viewModels<T> {
+  object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.activityViewModel(
+  crossinline provider: () -> T
+) = activityViewModels<T> {
   object : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
   }
